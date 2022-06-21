f1480f46ecf9a5646949cc57bf91f330d7e924e4
moc1n <1107rizkyeka@gmail.com>
2022-06-21 16:41:52 +0700

    SignUp page

---
+++ src/components/SignUp.vue
@@ -0,0 +1,48 @@
+<template>
+<img class="logo" src="../assets/RestoLogo-removebg-preview.png"/>
+<div class="register">
+<h1>Sign Up</h1>
+    <input type="text" name="" id="" placeholder="Enter Name" />
+    <input type="text" name="" id="" placeholder="Enter Email" />
+    <input type="Password" name="" id="" placeholder="Enter Password" />
+    <button> Sign in</button>
+</div>
+</template>
+
+<script>
+export default{
+    name:'SignUp'
+}
+</script>
+
+<style>
+.logo{
+    width:200px;
+    margin-bottom: 0px;
+}
+.register{
+    margin-top: -50px;
+}
+.register input{
+    width:300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+.register button {
+    margin-top: 0%;
+    border: 1px solid skyblue;
+    height: 30px;
+    width: 325px;
+    background: skyblue;
+    color: #ffffffff;
+    cursor: pointer;
+}
+.register button:hover {
+background-color: steelblue;
+}
+</style>
