88be4057b4470d18944ad23e16d0ca0f14c1cdd1
moc1n <1107rizkyeka@gmail.com>
2022-06-29 23:59:34 +0700

    Making simple signup Vue with json server api as db

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,16 +2,46 @@
 <img class="logo" src="../assets/RestoLogo-removebg-preview.png"/>
 <div class="register">
 <h1>Sign Up</h1>
-    <input type="text" name="" id="" placeholder="Enter Name" />
-    <input type="text" name="" id="" placeholder="Enter Email" />
-    <input type="Password" name="" id="" placeholder="Enter Password" />
-    <button> Sign in</button>
+    <!-- <input type="hidden" name="" id="" placeholder="Enter Name" /> -->
+    <input type="text" v-model="name" placeholder="Enter Name" />
+    <input type="text" v-model="email" placeholder="Enter Email" />
+    <input type="password" v-model="password" placeholder="Enter Password" />
+    <button v-on:click="SignUp"> Sign in</button>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default{
-    name:'SignUp'
+    name:'SignUp',
+    data(){
+        return {
+            // id:'',
+            name: '',
+            email:'',
+            password:''
+
+        }
+    },
+    methods:{
+        async SignUp()
+        {
+            // console.warn("signup",this.name,this.email,this.password);
+            let result = await axios.post("http://localhost:3000/users", 
+            {
+                email:this.email,
+                name:this.name,
+                password:this.password,
+            });
+            
+            console.warn(result);
+            if(result.status == 201){
+                alert("Sign-up Done.");
+                localStorage.setItem("User-info", JSON.stringify(result.data));
+            }
+
+        }
+    }
 }
 </script>
 
